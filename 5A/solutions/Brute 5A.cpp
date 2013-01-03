bb3e230535093f9e56bca8b2a693a981d6af4fb9
Borys Dibrov <dibrov.bor@gmail.com>
2013-01-03 21:58:26 +0200

    Outputs and statements has been added 5A 5F

---
+++ 5A/solutions/Brute 5A.cpp
@@ -0,0 +1,72 @@
+#pragma comment(linker, "/STACK:16777216")
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <cstdio>
+#include <cmath>
+#include <cctype>
+#include <cassert>
+#include <cstring>
+#include <cstdlib>
+#include <utility>
+#include <string>
+#include <map>
+#include <set>
+#include <vector>
+#include <queue>
+#include <ctime>
+#include <stack>
+#include <algorithm>
+#include <numeric>
+#include <complex>
+#include <functional>
+#include <list>
+#include <iostream>
+using namespace std;
+
+#define C(_a, _v) memset(_a,_v,sizeof(_a))
+#define ALL(_obj) (_obj).begin(),(_obj).end()
+#define FORB(_i,_a,_b) for((_i)=(_a);(_i)<(_b);++(_i))
+#define FORE(_i,_a,_b) for((_i)=(_a);(_i)<=(_b);++(_i))
+#define FOR(_i,_n) FORB(_i,0,_n)
+#define FORS(_i,_obj) FOR(_i,(_obj).size())
+#define ADJ(_i,_v) for((_i)=kick[_v];(_i)>=0;(_i)=foll[_i])
+#define X first
+#define Y second
+#define I64 "%lld"
+
+typedef long long i64;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef pair<i64, i64> pii64;
+typedef vector<pii> vpii;
+
+template<typename T>inline bool remin(T&c,const T&n){if(n<c){c=n;return 1;}return 0;}
+template<typename T>inline bool remin2(T&c,const T&n){if(c<0||n<c){c=n;return 1;}return 0;}
+template<typename T>inline bool remax(T&c,const T&n){if(c<n){c=n;return 1;}return 0;}
+template<typename T>inline void addmod(T&c,const T&n,const T&m){c = (c + n) % m;}
+
+int _in;int in(){scanf("%d",&_in);return _in;}
+
+// stuff cutline
+
+const int N = 1e5 + 5;
+
+i64 arr[N];
+
+int main() {
+  i64 sum = 0;
+  int i, n, q, a, b;
+  scanf("%d%d", &n, &q);
+  FOR (i, n) {
+    scanf(I64, arr + i);
+  }
+  while (--q >= 0) {
+    scanf("%d%d", &a, &b);
+    sum = 0;
+    for (i = a - 1; i < b; ++i) {
+      sum += arr[i];
+    }
+    printf(I64 "\n", sum);
+  }
+  return 0;
+}
